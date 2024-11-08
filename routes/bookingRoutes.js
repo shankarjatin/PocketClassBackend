9befb359317221b5fa14a7bb0070ccc8b7817ac8
shankarjatin <shankarjatin1005@gmail.com>
2024-11-08 13:08:37 +0530

    booking routes

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+const { authenticate, authorize } = require('../middleware/authMiddleware');
+
+router.post('/bookings', authenticate, authorize(['student']), bookingController.createBooking);
+router.get('/bookings/:instructorId', authenticate, authorize(['instructor']), bookingController.getConfirmedBookings);
+
+module.exports = router;
